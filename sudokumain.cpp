e867ad90e3ce3e149f63833d5af850f223e96d00
AlejandroVegaUR <94658310+AlejandroVegaUR@users.noreply.github.com>
2021-11-18 20:28:34 -0500

    Update sudokumain.cpp

--- sudokumain.cpp
+++ sudokumain.cpp
@@ -2,25 +2,25 @@
 
 int main(){
     
-    // vector<vector<int>> tablero {
+    vector<vector<int>> tablero2x2 { // tablero 2x2 por resolver
 
-    //     { 0 , 0 , 3 , 2 },
-    //     { 3 , 0 , 0 , 4 },
-    //     { 4 , 3 , 2 , 0 },
-    //     { 0 , 0 , 0 , 3 }
+        { 0 , 0 , 3 , 2 },
+        { 3 , 0 , 0 , 4 },
+        { 4 , 3 , 2 , 0 },
+        { 0 , 0 , 0 , 3 }
     
-    // };
+    };
 
-    // vector<vector<int>> solnesperada {
+    vector<vector<int>> solnesperada2x2 { // tablero 2x2 ya resuelto para verificar el algoritmo
 
-    //     { 1 , 4 , 3 , 2 },
-    //     { 3 , 2 , 1 , 4 },
-    //     { 4 , 3 , 2 , 1 },
-    //     { 2 , 1 , 4 , 3 }
+        { 1 , 4 , 3 , 2 },
+        { 3 , 2 , 1 , 4 },
+        { 4 , 3 , 2 , 1 },
+        { 2 , 1 , 4 , 3 }
 
-    // };
+    };
 
-    vector<vector<int>> tablero { // tablero por resolver
+    vector<vector<int>> tablero3x3 { // tablero 3x3 por resolver
 
         { 1 , 0 , 0 , 0 , 0 , 7 , 0 , 9 , 0 },
         { 0 , 3 , 0 , 0 , 2 , 0 , 0 , 0 , 8 },
@@ -34,7 +34,7 @@ int main(){
 
     };
 
-    vector<vector<int>> solnesperada { // tablero ya resuelto para verificar el algoritmo
+    vector<vector<int>> solnesperada3x3 { // tablero 3x3 ya resuelto para verificar el algoritmo
 
         { 1 , 6 , 2 , 8 , 5 , 7 , 4 , 9 , 3 },
         { 5 , 3 , 4 , 1 , 2 , 9 , 6 , 7 , 8 },
@@ -48,20 +48,20 @@ int main(){
 
     };
 
-    sudokusolver solver(3); // se crea la clase
+    sudokusolver solver(3); // se crea la clase, para usar el sudoku 2x2 se cambia por un dos este valor y se utilizan los tableros 2x2 en el resto del codigo
 
     cout << "tablero por resolver: \n";
-    solver.print(tablero);
+    solver.print(tablero3x3);
 
     cout << "solucion esperada: \n";
-    solver.print(solnesperada);
+    solver.print(solnesperada3x3);
 
-    solver.solucion(tablero); // se llama la funcion para resolver el tablero
+    solver.solucion(tablero3x3); // se llama la funcion para resolver el tablero
 
     cout << "tablero resuelto: \n";
-    solver.print(tablero);
+    solver.print(tablero3x3);
 
-    if(tablero == solnesperada){
+    if(tablero3x3 == solnesperada3x3){
 
         cout << "si se llego a la solucion\n";
 
